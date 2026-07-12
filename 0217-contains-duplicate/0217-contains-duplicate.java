d7ccf5cc481c3e397d9ccf1c43621b8dbe07cb56
jeetpal <jeetpal7067@gmail.com>
2026-07-13 00:59:16 +0530

    Time: 13 ms (93.01%), Space: 108.9 MB (5.06%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.java
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -1,14 +1,11 @@
+import java.util.HashSet;
 
-import java.util.HashMap;
 class Solution {
     public boolean containsDuplicate(int[] nums) {
-        HashMap<Integer, Integer> freq = new HashMap<>();
-        
-        for(int num : nums){
-            freq.put(num,freq.getOrDefault(num,0) + 1);
-            if(freq.get(num)>1) return true;
+        HashSet<Integer> seen = new HashSet<>();
+        for (int num : nums) {
+            if (!seen.add(num)) return true;
         }
         return false;
-
     }
 }
